14b6831b297a003245ee301660a203aa524f45a4
allisonaustin <64452109+allisonaustin@users.noreply.github.com>
2022-11-14 16:53:57 -0800

    updated tasks

--- userstudy/tasks/task_data.js
+++ userstudy/tasks/task_data.js
@@ -11,80 +11,80 @@ var taskDatasets = {
       },
       {
         "qtype": "identifying", // incorrect mapping - multiple inheritance
-        "question": "How many classes is \"Author\" in the left ontology mapped to?",
+        "question": "How many classes is \"Author\" (in the left ontology) mapped to?",
         "atype": "number",
         "options":["0", "1", "2", "3"],
         "answer": "1"
       },
       {
         "qtype": "identifying", // incorrect mapping
-        "question": "What is \"SlideSet\" in the left ontology mapped to in the right ontology?",
+        "question": "What is \"SlideSet\" (in the left ontology) mapped to (in the right ontology)?",
         "atype": "class",
         "answer": "Document"
       },
       {
         "qtype": "validation", // correct mapping
-        "question": "Is there a mapping between \"AcademicEvent\" in the left ontology and \"Scientific_Event\" in the right ontology?",
+        "question": "Is there a mapping between \"AcademicEvent\" (in the left ontology) and \"Scientific_Event\" (in the right ontology)?",
         "atype": "y/n",
         "answer": "Yes"
       },
       {
         "qtype": "validation", // correct mapping
-        "question": "Is \"AcademiaOrganization\" in the left ontology correctly mapped?",
+        "question": "Is \"AcademiaOrganization\" (in the left ontology) correctly mapped?",
         "atype": "y/n",
         "answer": "Yes"
       },
       {
         "qtype": "identifying", // missing mapping
-        "question": "Can \"Person\" in the left ontology be mapped to another class in the right ontology?",
+        "question": "Can \"Person\" (in the left ontology) be mapped to another class (in the right ontology)?",
         "atype": "y/n",
         "answer": "Yes"
       },
       {
         "qtype": "creation", // missing mapping
-        "question": "Which class could \"Attendee\" in the left ontology be mapped to in the right ontology?",
+        "question": "Which class could \"Attendee\" (in the left ontology) be mapped to (in the right ontology)?",
         "atype": "class",
         "answer": "Conference_Participant"
       },
       {
         "qtype": "identifying", // correct mapping
-          "question": "What is \"ConferenceDinner\" in the left ontology mapped to in the right ontology?",
+          "question": "Which class could \"ConferenceDinner\" (in the left ontology) mapped to (in the right ontology)?",
           "atype": "class",
           "answer": "Conference_Banquet"
         },
       {
         "qtype": "validation", // incorrect mapping
-        "question": "\"SecurityTopic\" in the left ontology is mapped to \"Research_Topic\" in the right ontology. Is this a correct mapping?",
+        "question": "\"SecurityTopic\" (in the left ontology) is mapped to \"Research_Topic\" (in the right ontology). Is this correct?",
         "atype": "y/n",
         "answer": "No"
       },
       {
         "qtype": "validation", // correct mapping
-        "question": "\"Place\" in the left ontology is mapped to \"Location\" in the right ontology. Is this a correct mapping?",
+        "question": "\"Place\" (in the left ontology) is mapped to \"Location\" (in the right ontology). Is this correct?",
         "atype": "y/n",
         "answer": "Yes"
       },
       {
         "qtype": "identifying", // incorrect mapping
-        "question": "Is the mapping from \"RejectedPaper\" in the left ontology to \"Assigned_Paper\" in the right ontology correct?",
+        "question": "\"RejectedPaper\" (in the left ontology) is mapped to \"Assigned_Paper\" (in the right ontology). Is this correct?",
         "atype": "y/n",
         "answer": "No"
       },
       {
         "qtype": "validation", // incorrect mapping
-        "question": "Is the mapping from \"IndustryOrganization\" in the left ontology to \"Organisation\" in the right ontology correct?",
+        "question": "\"IndustryOrganization\" (in the left ontology) is mapped to \"Organisation\" (in the right ontology). Is this correct?",
         "atype": "y/n",
         "answer": "No"
       },
       {
         "qtype": "creation", // missing mapping
-        "question": "What class can \"AcceptedPaper\" in the left ontology be mapped to in the right ontology?",
+        "question": "Which class could \"AcceptedPaper\" (in the left ontology) be mapped to (in the right ontology)?",
         "atype": "class",
         "answer": "Accepted_Paper"
       },
       {
         "qtype": "identifying", // missing mapping
-        "question": "Is there a mapping that should exist between \"Workshop\” in the left ontology to another class in the right ontology?",
+        "question": "Can \"Workshop\” (in the left ontology) be mapped to another class (in the right ontology)?",
         "atype": "y/n",
         "answer": "Yes"
       },
@@ -108,80 +108,80 @@ var taskDatasets = {
       },
       {
         "qtype": "identifying", // correct mapping
-        "question": "How many classes is \"Skin\" in the left ontology mapped to?",
+        "question": "How many classes is \"Skin\" (in the left ontology) mapped to?",
         "atype": "number",
         "options": ["0", "1", "2", "4"],
         "answer": "1"
       },
       {
         "qtype": "identifying", // correct mapping
-        "question": "What is \"Viscera\" in the left ontology mapped to in the right ontology?",
+        "question": "What is \"Viscera\" (in the left ontology) mapped to (in the right ontology)?",
         "atype": "class",
         "answer": "visceral organ system"
       },
       {
         "qtype": "identifying", // missing mapping
-        "question": "Is there a mapping between \"Blood\" in the left ontology and \"blood\" in the right ontology?",
+        "question": "Is there a mapping between \"Blood\" (in the left ontology) and \"blood\" (in the right ontology)?",
         "atype": "y/n",
         "answer": "No"
       },
       {
         "qtype": "validation", // incorrect mapping
-        "question": "Is \"Cartilage\" in the left ontology correctly mapped?",
+        "question": "Is \"Cartilage\" (in the left ontology) correctly mapped?",
         "atype": "y/n",
         "answer": "No"
       },
       {
         "qtype": "identifying", // missing mapping
-        "question": "Can \"Joint\" in the left ontology be mapped to another class in the right ontology?",
+        "question": "Can \"Joint\" (in the left ontology) be mapped to another class (in the right ontology)?",
         "atype": "y/n",
         "answer": "Yes"
       },
       {
         "qtype": "creation", // missing mapping
-        "question": "Which class could \"Heart\" in the left ontology be mapped to in the right ontology?",
+        "question": "Which class could \"Heart\" (in the left ontology) be mapped to (in the right ontology)?",
         "atype": "class",
         "answer": "heart"
       },
       {
         "qtype": "identifying", // correct mapping
-        "question": "What is \"Skull\" in the left ontology mapped to in the right ontology?",
+        "question": "What is \"Skull\" (in the left ontology) mapped to (in the right ontology)?",
         "atype": "class",
         "answer": "cranium"
       },
       {
         "qtype": "validation", // incorrect mapping
-        "question": "\"Urinary_System_Part\" in the left ontology is mapped to \"muscle\" in the right ontology. Is this a correct mapping?",
+        "question": "\"Urinary_System_Part\" (in the left ontology) is mapped to \"muscle\" (in the right ontology). Is this correct?",
         "atype": "y/n",
         "answer": "No"
       },
       {
         "qtype": "validation", // incorrect mapping
-        "question": "\"Cheek\" in the left ontology is mapped to \"cuticle\" in the right ontology. Is this a correct mapping?",
+        "question": "\"Cheek\" (in the left ontology) is mapped to \"cuticle\" (in the right ontology). Is this correct?",
         "atype": "y/n",
         "answer": "No"
       },
       {
         "qtype": "identifying", // correct mapping
-        "question": "Is the mapping from \"Skin\" in the left ontology to \"skin\" in the right ontology correct?",
+        "question": "\"Skin\" (in the left ontology) is mapped to \"skin\" (in the right ontology). Is this correct?",
         "atype": "y/n",
         "answer": "Yes"
       },
       {
         "qtype": "validation", // incorrect mapping
-        "question": "Is the mapping from \"Mucus\" in the left ontology to \"nasal mucus\" in the right ontology correct?",
+        "question": "\"Mucus\" (in the left ontology) is mapped to \"nasal mucus\" (in the right ontology). Is this correct?",
         "atype": "y/n",
         "answer": "No"
       },
       {
         "qtype": "creation", // missing mapping
-        "question": " What class can \"Lip\" in the left ontology be mapped to in the right ontology?",
+        "question": "Which class could \"Lip\" (in the left ontology) be mapped to (in the right ontology)?",
         "atype": "class",
         "answer": "Lip"
       },
       {
         "qtype": "identifying", // missing mapping
-        "question": "Is there a mapping that should exist between \”Arm\” in the left ontology to another class in the right ontology?",
+        "question": "Can \”Arm\” (in the left ontology) be mapped to another class (in the right ontology)?",
         "atype": "y/n",
         "answer": "Yes"
       },
